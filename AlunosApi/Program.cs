8b7edccec87b28461a87d368c31280547e9dfbac
Diogo Lessa <diogodslessa@gmail.com>
2022-07-18 22:01:54 -0300

    Acesso a dados

--- AlunosApi/Program.cs
+++ AlunosApi/Program.cs
@@ -1,4 +1,5 @@
 using AlunosApi.Context;
+using AlunosApi.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IAlunoService, AlunosService>();
 
 builder.Services.AddControllers();
 
