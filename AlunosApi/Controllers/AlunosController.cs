96e2268fea71500b518c95e2ec5879637e1881bc
Diogo Lessa <diogodslessa@gmail.com>
2022-07-18 22:05:47 -0300

    Controller começado

---
+++ AlunosApi/Controllers/AlunosController.cs
@@ -0,0 +1,17 @@
+﻿using AlunosApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosController : ControllerBase
+    {
+        private readonly IAlunoService _service;
+        public AlunosController(IAlunoService service)
+        {
+            _service = service;
+        }
+    }
+}
